7b8570e3575e178ffbd6a076ac6efc8398bdbc0a
Kevin Peterson <kevin.peterson@mayo.edu>
2011-12-19 20:43:53 -0600

    Ignore test.

--- bioportal-service/src/test/groovy/edu/mayo/cts2/framework/plugin/service/bioportal/integration/EntityDescriptionReadTestIT.groovy
+++ bioportal-service/src/test/groovy/edu/mayo/cts2/framework/plugin/service/bioportal/integration/EntityDescriptionReadTestIT.groovy
@@ -2,12 +2,14 @@ package edu.mayo.cts2.framework.plugin.service.bioportal.integration;
 
 import static org.junit.Assert.*
 
+import org.junit.Ignore
 import org.junit.Test
 
 import edu.mayo.cts2.framework.model.entity.EntityDescriptionMsg
 
 class EntityDescriptionReadTestIT extends BaseServiceTestITBase {
 	
+	@Ignore
 	@Test void TestReadEntityWithColon(){
 
 		EntityDescriptionMsg result = 
