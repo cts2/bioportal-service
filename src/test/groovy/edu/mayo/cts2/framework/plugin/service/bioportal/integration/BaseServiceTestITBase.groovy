acf3faa25ca60449cd16dcf37cd33e33bb36aa8d
Kevin Peterson <kevin.peterson@mayo.edu>
2012-01-26 11:17:06 -0600

    Added wait time for integration tests.

--- src/test/groovy/edu/mayo/cts2/framework/plugin/service/bioportal/integration/BaseServiceTestITBase.groovy
+++ src/test/groovy/edu/mayo/cts2/framework/plugin/service/bioportal/integration/BaseServiceTestITBase.groovy
@@ -1,5 +1,9 @@
 package edu.mayo.cts2.framework.plugin.service.bioportal.integration;
 
+import org.junit.Before
+import org.springframework.http.HttpStatus
+import org.springframework.web.client.HttpStatusCodeException
+
 import edu.mayo.cts2.framework.core.client.Cts2RestClient
 
 
@@ -9,5 +13,28 @@ class BaseServiceTestITBase {
 	
 	public String server = "http://localhost:5150/webapp-rest/"
 	
-
+	//Wait until OSGi service installs itself and becomes available
+	@Before
+	void waitForService(){
+		boolean wait = true;
+		
+		long startTime = System.currentTimeMillis()
+		while(wait){
+			if( (System.currentTimeMillis() - startTime) > (20*1000)){
+				throw new RuntimeException("Timeout waiting for service to start")
+			}
+			try {
+				client.getCts2Resource(server + "/codesystems", Object)
+				} catch (HttpStatusCodeException e){
+					if(e.getStatusCode().equals(HttpStatus.NOT_IMPLEMENTED)){
+						Thread.currentThread().sleep(5000)
+						continue
+					} else {
+						throw e
+					}
+				}
+				
+			wait = false;
+		}
+	}
 }
